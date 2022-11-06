7825c712e9c5ee2f78c8db86e35e0feef1189b4a
LMXZ1258 <1078897881@qq.com>
2022-11-06 11:09:29 +0800

    Update script.js

--- blog/js/script.js
+++ blog/js/script.js
@@ -1,3 +1,8 @@
+const title= document.querySelector('#title1');
+const prev= document.querySelector('.prev');
+const clannad= document.querySelector('.clannad');
+const next= document.querySelector('.next');
+
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
@@ -24,3 +29,47 @@ window.onscroll = () =>{
   searchIcon.classList.remove('fa-times');
   searchForm.classList.remove('active');
 }
+
+
+
+
+let a = 1;
+let k = 1;
+
+const change = () => {
+  text = '//player.bilibili.com/player.html?aid=2899526&bvid=BV1ks41117be&cid=4547795&page='+k;
+  clannad.setAttribute("src",text);
+  title.innerHTML = "第"+a+"-"+k+"集";
+}
+
+//previous play
+const prevPlay = () => {
+    k--;
+    if (k < 0 && a == 1){
+        k = 22;
+        a = 2;
+    }
+    if (k = 0 && a == 2){
+      k = 22;
+      a = 1;
+    }
+    change();
+}
+
+prev.addEventListener("click", prevPlay);
+
+//next play
+const nextPlay = () => {
+    k++;
+    if (k = 23 && a == 1){
+        k = 1;
+        a = 2;
+    }
+    if (k = 23 && a == 2){
+        k = 1;
+        a = 1;
+    }
+    change();
+}
+
+next.addEventListener("click", nextPlay); 
