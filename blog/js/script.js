16907e11e97ad261e9a7aa476f6030cf74f61835
LMXZ1258 <1078897881@qq.com>
2022-11-07 20:19:03 +0800

    Update script.js

--- blog/js/script.js
+++ blog/js/script.js
@@ -4,7 +4,8 @@ const clannad= document.querySelector('.clannad');
 const next= document.querySelector('.next');
 const ja = document.querySelector('#jump_a');
 const jk = document.querySelector('#jump_k');
-const jbtn = document.querySelector('#jump_btn')
+const jbtn = document.querySelector('#jump_btn');
+const sc = window.screen.width;
 
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
@@ -38,6 +39,11 @@ let a = 1;
 let k = 1;
 
 const change = (m,n,q) => {
+  text = '//player.bilibili.com/player.html?aid=2899526&bvid=BV1ks41117be&cid='+q;
+  clannad.setAttribute('src',text);
+  title.innerHTML = "第"+m+"-"+n+"集";
+}
+const Change = (m,n,q) => {
   text = '//player.bilibili.com/player.html?aid=2899526&bvid=BV1ks41117be&cid=4547795&page='+q;
   clannad.setAttribute('src',text);
   title.innerHTML = "第"+m+"-"+n+"集";
@@ -56,8 +62,16 @@ const prevPlay = () => {
       k = 22;
       a = 1;
     }
-    ans = (a-1)*24+k;
-    change(a,k,ans);
+    if (sc<800)
+    {
+      ans =4547794+ (a-1)*24+k;
+      change(a,k,ans);
+  
+    }
+    else{
+      ans = (a-1)*24+k;
+      Change(a,k,ans);  
+    }
 }
 
 prev.addEventListener("click", prevPlay);
@@ -72,10 +86,19 @@ const nextPlay = () => {
     if (k == 23 && a == 2){
         k = 1;
         a = 1;
-    }
-    ans = (a-1)*24+k;
 
+}
+  if (sc<800)
+  {
+    ans =4547794+ (a-1)*24+k;
     change(a,k,ans);
+
+  }
+  else{
+    ans = (a-1)*24+k;
+    Change(a,k,ans);  
+  }
+
 }
 
 next.addEventListener("click", nextPlay); 
@@ -83,9 +106,16 @@ next.addEventListener("click", nextPlay);
 const jump_video = () => {
   k = Number(jk.value);
   a = Number(ja.value);
-  ans = (a-1)*24+k;
+  if (sc<800)
+  {
+    ans =4547794+ (a-1)*24+k;
+    change(a,k,ans);
 
-  change(a,k,ans);
+  }
+  else{
+    ans = (a-1)*24+k;
+    Change(a,k,ans);  
+  }
 }
 
 jbtn.addEventListener("click",jump_video);
