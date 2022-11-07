36544787894cbf745cd75701ba9e467672e25198
LMXZ1258 <1078897881@qq.com>
2022-11-07 17:38:19 +0800

    Update script.js

--- blog/js/script.js
+++ blog/js/script.js
@@ -2,6 +2,9 @@ const title= document.querySelector('#title1');
 const prev= document.querySelector('.prev');
 const clannad= document.querySelector('.clannad');
 const next= document.querySelector('.next');
+const ja = document.querySelector('#jump_a');
+const jk = document.querySelector('#jump_k');
+const jbtn = document.querySelector('#jump_btn')
 
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
@@ -34,8 +37,8 @@ window.onscroll = () =>{
 let a = 1;
 let k = 1;
 
-const change = (m,n) => {
-  text = '//player.bilibili.com/player.html?aid=2899526&bvid=BV1ks41117be&cid=4547795&page='+n;
+const change = (m,n,q) => {
+  text = '//player.bilibili.com/player.html?aid=2899526&bvid=BV1ks41117be&cid=4547795&page='+q;
   clannad.setAttribute('src',text);
   title.innerHTML = "第"+m+"-"+n+"集";
 }
@@ -53,7 +56,8 @@ const prevPlay = () => {
       k = 22;
       a = 1;
     }
-    change(a,k);
+    ans = (a-1)*24+k;
+    change(a,k,ans);
 }
 
 prev.addEventListener("click", prevPlay);
@@ -69,7 +73,19 @@ const nextPlay = () => {
         k = 1;
         a = 1;
     }
-    change(a,k);
+    ans = (a-1)*24+k;
+
+    change(a,k,ans);
 }
 
 next.addEventListener("click", nextPlay); 
+
+const jump_video = () => {
+  k = Number(jk.value);
+  a = Number(ja.value);
+  ans = (a-1)*24+k;
+
+  change(a,k,ans);
+}
+
+jbtn.addEventListener("click",jump_video);
